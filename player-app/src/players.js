3c7a05db161edfc2380a24f332ea99592c353547
Ambayal <ambocoum@gmail.com>
2023-10-18 16:17:27 +0000

    first commit

---
+++ player-app/src/players.js
@@ -0,0 +1,38 @@
+// src/players.js
+const players = [
+    {
+      name: 'Player 1',
+      team: 'Team A',
+      nationality: 'Country A',
+      jerseyNumber: 10,
+      age: 25,
+      imageUrl: 'URL to Player 1 Image',
+    },
+    {
+      name: 'Player 2',
+      team: 'Team B',
+      nationality: 'Country B',
+      jerseyNumber: 7,
+      age: 28,
+      imageUrl: 'URL to Player 2 Image',
+    },
+    {
+      name: 'Player 3',
+      team: 'Team A',
+      nationality: 'Country C',
+      jerseyNumber: 5,
+      age: 23,
+      imageUrl: 'URL to Player 3 Image',
+    },
+    {
+      name: 'Player 4',
+      team: 'Team B',
+      nationality: 'Country A',
+      jerseyNumber: 9,
+      age: 30,
+      imageUrl: 'URL to Player 4 Image',
+    },
+  ];
+  
+  export default players;
+  
